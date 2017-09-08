dcce4642c10817136270a0449d8faf6d5c87735b
Евгений Корсуков <nickarsar@gmail.com>
2017-09-08 12:56:48 +0600

    First commit

---
+++ mysql.install.sql
@@ -0,0 +1,2 @@
+INSERT INTO `#__extensions` (`name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `manifest_cache`, `params`, `custom_data`, `system_data`, `checked_out`, `checked_out_time`, `ordering`, `state`) VALUES
+('plg_j2store_payment_platron', 'plugin', 'payment_platron', 'j2store', 0, 1, 1, 0, '', '', '', '', 0, '2014-10-11 16:41:55', 0, 0);
---
+++ plugins/j2store/payment_platron/payment_platron/tmpl/prepayment.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * --------------------------------------------------------------------------------
+   Weblogicx India  - J2 Store v 3.0 - Payment Plugin - SagePay
+ * --------------------------------------------------------------------------------
+ * @package		Joomla! 2.5x
+ * @subpackage	J2 Store
+ * @author    	Weblogicx India http://www.weblogicxindia.com
+ * @copyright	Copyright (c) 2010 - 2015 Weblogicx India Ltd. All rights reserved.
+ * @license		GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link		http://weblogicxindia.com
+ * --------------------------------------------------------------------------------
+*/
+
+//no direct access
+defined('_JEXEC') or die('Restricted access'); 
+
+
+
+?>
+
+<style type="text/css">
+    #platron_form { width: 100%; }
+    #platron_form td { padding: 5px; }
+    #platron_form .field_name { font-weight: bold; }
+</style>
+
+<form action="<?php echo JRoute::_( "https://paybox.kz/payment.php" ); ?>" method="post" name="adminForm" enctype="multipart/form-data">
+
+	<?foreach($vars as $name => $value){?>
+		<input type='hidden' name='<?echo $name?>' value='<?echo $value?>'>
+    <?}?>
+            
+    <input type="submit" class="btn btn-primary button" value="<?php echo JText::_('J2STORE_SAGEPAY_CLICK_TO_COMPLETE_ORDER'); ?>" />
+</form>
