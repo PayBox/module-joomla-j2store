8265ca427531eb53246921e2f33fb47c3902a195
kap1c <ggkapic@mail.ru>
2020-02-26 00:10:50 +0600

    modified for v3

--- plugins/j2store/payment_paybox/payboxv2.php
+++ plugins/j2store/payment_paybox/payboxv2.php
@@ -2,8 +2,8 @@
 /*------------------------------------------------------------------------
 # com_j2store - J2Store
 # ------------------------------------------------------------------------
-# author    Ramesh Elamathi - Weblogicx India http://www.weblogicxindia.com
-# copyright Copyright (C) 2014 - 19 Weblogicxindia.com. All Rights Reserved.
+# author    Galym Sarsebek - PayBox Money  https://www.paybox.money
+# copyright Copyright (C) 2020 paybox.money All Rights Reserved.
 # @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
 # Websites: http://j2store.org
 # Technical Support:  Forum - http://j2store.org/forum/index.html
